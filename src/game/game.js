74df08a8b43c7afe30352ad11726d6eb1089aff0
James Chambless <j.chambless@gmail.com>
2023-09-20 12:15:50 -0400

    Clean up promise since not used

--- src/game/game.js
+++ src/game/game.js
@@ -2,21 +2,19 @@ import { Game, AUTO, Scale } from 'phaser'
 import { MainScene } from './scenes/MainScene'
 
 export function launch() {
-  return new Promise((resolve, reject) => {
-    resolve(new Game({
-      type: AUTO,
-      scale: {
-        mode: Scale.RESIZE,
-        width: window.innerWidth * window.devicePixelRatio,
-        autoCenter: Scale.CENTER_BOTH,
-        height: window.innerHeight * window.devicePixelRatio,
-      },
-      parent: "game",
-      backgroundColor: "#201726",
-      physics: {
-        default: "arcade"
-      },
-      scene: MainScene
-    }));
+  new Game({
+    type: AUTO,
+    scale: {
+      mode: Scale.RESIZE,
+      width: window.innerWidth * window.devicePixelRatio,
+      autoCenter: Scale.CENTER_BOTH,
+      height: window.innerHeight * window.devicePixelRatio,
+    },
+    parent: "game",
+    backgroundColor: "#201726",
+    physics: {
+      default: "arcade"
+    },
+    scene: MainScene
   });
 }
